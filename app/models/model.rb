8075df534d0a08b25b3243dd336a3f6695f89ac4
Gonzalez32 <3232rose@gmail.com>
2021-03-11 12:24:10 -0600

    modified files for devise

---
+++ app/models/model.rb
@@ -0,0 +1,7 @@
+class Model < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  before_action :authenticate_user!
+
+end
